9a805f5e64845a57f49e2f438c19121eba15dd57
Bruno Barbosa <br.barbosa.souza1@gmail.com>
2025-12-04 23:12:52 -0300

    correção

--- src/main/java/dev/bruno/PersonRegistry/repositorys/PersonRepository.java
+++ src/main/java/dev/bruno/PersonRegistry/repositorys/PersonRepository.java
@@ -3,5 +3,6 @@ package dev.bruno.PersonRegistry.repositorys;
 import dev.bruno.PersonRegistry.model.PersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PersonRepository extends JpaRepository<PersonModel, Long> {
 }
